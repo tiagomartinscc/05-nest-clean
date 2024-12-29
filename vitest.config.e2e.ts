445b51cdca01a1d5acc0377ae184e03ea5dbbec1
Tiago Martins <tiagomartinscc@hotmail.com>
2024-12-29 15:50:52 -0300

    create a suite of tests with a new schema database

---
+++ vitest.config.e2e.ts
@@ -0,0 +1,12 @@
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    include: ['**/*.e2e-spec.ts'],
+    globals: true,
+    root: './src',
+    setupFiles: ['./test/setup-e2e.ts'],
+  },
+  plugins: [swc.vite()],
+})
