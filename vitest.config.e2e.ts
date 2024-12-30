5e145b1785fa5d978f4d4b44eb1838439d09876d
Tiago Martins <tiagomartinscc@hotmail.com>
2024-12-30 06:08:26 -0300

    e2e user testing

--- vitest.config.e2e.ts
+++ vitest.config.e2e.ts
@@ -1,12 +1,18 @@
 import swc from 'unplugin-swc'
 import { defineConfig } from 'vitest/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   test: {
     include: ['**/*.e2e-spec.ts'],
     globals: true,
-    root: './src',
+    root: './',
     setupFiles: ['./test/setup-e2e.ts'],
   },
-  plugins: [swc.vite()],
+  plugins: [
+    tsConfigPaths(),
+    swc.vite({
+      module: { type: 'es6' },
+    }),
+  ],
 })
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,6 @@
 import swc from 'unplugin-swc'
 import { defineConfig } from 'vitest/config'
+import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   test: {
@@ -7,9 +8,8 @@ export default defineConfig({
     root: './',
   },
   plugins: [
-    // This is required to build the test files with SWC
+    tsConfigPaths(),
     swc.vite({
-      // Explicitly set the module type to avoid inheriting this value from a `.swcrc` config file
       module: { type: 'es6' },
     }),
   ],
